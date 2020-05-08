91cde17498fc8fae6d5a0c7395e319781ffaa86f
Davids.berz <Davids.berz@gmail.com>
2020-05-08 09:38:36 -0500

    ms-eureka

---
+++ src/test/java/com/mservice/app/eureja/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mservice.app.eureja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
